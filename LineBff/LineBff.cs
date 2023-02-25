d01a6672efced80c29a364049773d9eb6a6d1b83
KUGAKU <githubgemini.0521@gmail.com>
2023-02-25 11:16:38 +0900

    first commit

---
+++ LineBff/LineBff.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LineBff
+{
+    public class LineBff
+    {
+        private readonly ILogger _logger;
+
+        public LineBff(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<LineBff>();
+        }
+
+        [Function("LineBff")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
+
